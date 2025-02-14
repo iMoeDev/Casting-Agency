1f28305d7bce5f5f11974512df51ddffae660d40
mohammed akif <mmakif@stu.kau.edu.sa>
2025-02-14 17:36:42 +0300

    fix env

--- setup.sh
+++ setup.sh
@@ -4,4 +4,7 @@ export AUTH0_DOMAIN="dev-hsd210wqnr1cmu4f.us.auth0.com" # Enter your AUTH0_DOMAI
 export API_AUDIENCE='coffee' #Enter your API Audience
 export DB_URL="postgresql://postgres:123@localhost:5432/agency" #Change username & password
 export DB_TEST_URL="postgresql://postgres:123@localhost:5432/casting_test" #Change username & password
+export REACT_APP_AUTH0_DOMAIN=dev-hsd210wqnr1cmu4f.us.auth0.com
+export REACT_APP_AUTH0_CLIENT_ID=XB9N8QSz5Js70hHK9aDLVelEb0pCTGbT
+export REACT_APP_AUTH0_AUDIENCE=coffee
 
