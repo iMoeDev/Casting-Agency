15623752e173408d5a1c1da26ea9deed06023454
mohammed akif <mmakif@stu.kau.edu.sa>
2025-02-14 17:30:08 +0300

    fix building

--- backend/manage.py
+++ backend/manage.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__))) 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
